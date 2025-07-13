61732a333aab7226d3bb2a5959172ee11f292125
ManvithaDungi <manvitha3626@gmail.com>
2025-07-14 01:34:07 +0530

    package json error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// src/App.js
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom';
-
 
 function App() {
   return (
